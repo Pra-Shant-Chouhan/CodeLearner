9508268d3a0ad88308bca24565e6d68c241fc483
PRASHANT CHOUHAN <pchouhan122@gmail.com>
2025-04-29 23:51:37 +0530

    mongodb connection work.

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import dotenv from "dotenv"
+import connectDB from './db/DB.js';
 
 dotenv.config({
-    path:"../.env"
+    path: "../.env"
 })
 
-const PORT =process.env.PORT || 8000
+const PORT = process.env.PORT || 8000
 const app = express();
 
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+    })
+    .catch((err) => {
+        console.error("Mongodb connection error", err);
+        process.exit(1); //not needed but did there
+    })
 export default app;
